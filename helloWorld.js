97a5e0ac2f8d92b71aa895faabdf05065eb1dcd4
tnlink <tnlink@outlook.com>
2015-03-16 22:28:08 +0000

    set localhost to be the host name of node.js server

--- helloWorld.js
+++ helloWorld.js
@@ -41,5 +41,5 @@ http.createServer(function (req, res) {
 		  }
 		  res.end();
     }
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+}).listen(1337);
+console.log('Server running at http://localhost:1337/');
